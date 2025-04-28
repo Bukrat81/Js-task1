b0c7f909ae42a58002ea467ebcc0e72566198953
Bukrat81 <bukrat81@gmail.com>
2025-04-28 13:25:38 +0400

    Add files via upload

---
+++ Task2JS.js
@@ -0,0 +1,6 @@
+
+let firstName = prompt("Enter your first name:");
+let lastName = prompt("Enter your surname:");
+
+
+alert("Salam, " + firstName + " " + lastName + "!");
---
+++ task1js.js
@@ -0,0 +1,9 @@
+let digit1 = prompt("Birinci reqem:");
+let digit2 = prompt("Ikinci reqem:");
+let digit3 = prompt("Ucuncu reqem:");
+let digit4 = prompt("Dorduncu reqem:");
+
+let sum = parseInt(digit1) + parseInt(digit2) + parseInt(digit3) + parseInt(digit4);
+
+
+alert("Cemi: " + sum);
